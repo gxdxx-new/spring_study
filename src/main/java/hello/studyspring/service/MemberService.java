9a0709859beae60469bee27737fda22655bfb1c3
남기돈 <nkd0310@naver.com>
2021-02-08 11:31:49 +0900

    회원 서비스 개발

---
+++ src/main/java/hello/studyspring/service/MemberService.java
@@ -0,0 +1,45 @@
+package hello.studyspring.service;
+
+import hello.studyspring.domain.Member;
+import hello.studyspring.repository.MemberRepository;
+import hello.studyspring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원 가입
+     */
+    public Long join(Member member) {
+
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    //반환값이 Optional<>일 경우만 ifPresent 가능
+    //ctrl+alt+shift+T -> Extract method
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+
+}
