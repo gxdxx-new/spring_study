7a82f3ac7c66ff123187813060df4167e7dc749e
남기돈 <nkd0310@naver.com>
2021-02-10 16:21:52 +0900

    회원 웹 기능 - 등록, 홈 화면 설명 추가

--- src/main/java/hello/studyspring/controller/HomeController.java
+++ src/main/java/hello/studyspring/controller/HomeController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController {
+public class HomeController { //static에 index.html을 무시하고 home.html을 실행시킴
 
     @GetMapping("/")
     public String home() {
-        return "home";
+        return "home"; //home.html 호충
     }
 }
--- src/main/java/hello/studyspring/controller/MemberController.java
+++ src/main/java/hello/studyspring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.studyspring.controller;
 
+import hello.studyspring.domain.Member;
 import hello.studyspring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +15,27 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    /* home.html에서 회원가입 버튼을 누르면 /members/new로 이동함.
+       -> createMemberForm.html이 실행되고 사용자가 이름을 입력하면
+          /members/new에 Post방식으로 넘어옴.
+     */
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        //createMemberForm.html에서 저장된 name을 입력받아서 memberform에 저장
+        member.setName(form.getName());
+
+        //회원가입
+        memberService.join(member);
+
+        //홈 화면으로 이동
+        return "redirect:/";
     }
 }
